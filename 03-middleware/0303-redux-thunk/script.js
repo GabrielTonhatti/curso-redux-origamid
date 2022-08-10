c2458be132853e32dae59c8a2d95b9ff5740431b
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2022-08-09 21:54:35 -0300

    Redux Thunk

---
+++ 03-middleware/0303-redux-thunk/script.js
@@ -0,0 +1,52 @@
+const initialState = {
+    loading: false,
+    data: null,
+    error: null,
+};
+
+function reducer(state = initialState, action) {
+    console.log("Reducer");
+    switch (action.type) {
+        case "FETCH_STARTED":
+            return { ...state, loading: true };
+        case "FETCH_SUCCESS":
+            return { data: action.payload, loading: false, error: null };
+        case "FETCH_ERROR":
+            return { data: null, loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
+
+const thunk = (store) => (next) => (action) => {
+    if (typeof action === "function") {
+        return action(store.dispatch, store.getState);
+    }
+
+    return next(action);
+};
+
+const { applyMiddleware, compose } = Redux;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancers = composeEnhancers(applyMiddleware(thunk));
+const store = Redux.createStore(reducer, enhancers);
+
+function incrementar() {
+    return { type: "INCREMENTAR" };
+}
+
+function fetchUrl(url) {
+    return async (dispatch) => {
+        try {
+            dispatch({ type: "FETCH_STARTED" });
+
+            const data = await fetch(url).then((response) => response.json());
+            dispatch({ type: "FETCH_SUCCESS", payload: data });
+        } catch (error) {
+            dispatch({ type: "FETCH_ERROR", payload: error.message });
+        }
+    };
+}
+
+store.dispatch(fetchUrl("https://dogsapi.origamid.dev/json/api/photo"));
+store.dispatch(incrementar());
