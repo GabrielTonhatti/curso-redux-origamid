79857764151c1afadc6e72e061861512a6bdc5e1
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2022-08-24 13:56:45 -0300

    Desafio react/redux 2

---
+++ 04-react-redux/0408-react-redux-desafio-2/src/Components/Content.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Loading from "../Helper/Loading";
+import Login from "./Login";
+import Photos from "./Photos";
+
+const Content = () => {
+    const { user, token } = useSelector((state) => state.login);
+
+    if (user.loading || token.loading) return <Loading />;
+    if (user.data) return <Photos />;
+
+    return (
+        <div>
+            <Login />
+        </div>
+    );
+};
+
+export default Content;
